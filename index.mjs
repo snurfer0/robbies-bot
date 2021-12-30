2351632c25ccf9dd61eb23288ab9de145ee1a2af
Andrei Balanuta <andreimain03@gmail.com>
2021-12-30 04:46:59 +0200

    sasati la pidari

--- index.mjs
+++ index.mjs
@@ -2,36 +2,52 @@ import fetch from "node-fetch";
 
 // 12 robbies per request
 // counter is the total number of pages
-// 12 async requests needed to get all robbies
+// 100+ async requests needed to get all robbies
 // robbies NFT Contract 0xd4220b0b196824c2f548a34c47d81737b0f6b5d6
+// decimals 18
+// remove last 18 chars from string str.slice(0, -18)
+
+const BASE_URL = 'https://marketplace.biswap.org/back/offers/main-page'
+const DEFAULT_PARTNER = '6184f5111329baa2cd24ec38'
+const DEFAULT_FILTER = 'none'
+const DEFAULT_SORT = 'newest'
+const USER_ADDRESS = 'no-address'
+
+const getUrl = (sortBy, userAddress, page, partner, filter) => {
+    return `${BASE_URL}?sortBy=${sortBy}&userAddress=${userAddress}&page=${page}&partner=${partner}&filter=${filter}`
+}
+
+const request = async page => {
+    console.log(`Fetching robbies from page ${page}`)
+    const URL = getUrl(DEFAULT_SORT, USER_ADDRESS, page, DEFAULT_PARTNER, DEFAULT_FILTER)
+    const response = await fetch(URL, { method: 'GET' })
+    return await response.json()
+}
 
 const query = async () => {
 
     var robbies = []
+    var promises = []
 
-    const sortBy = 'newest'
-    const userAddress = 'no-address'
-    const page = '0'
-    const partner = '6184f5111329baa2cd24ec38'
-    const filter = 'none'
-
-    const url = `https://marketplace.biswap.org/back/offers/main-page?sortBy=${sortBy}&userAddress=${userAddress}&page=${page}&partner=${partner}&filter=${filter}`
-    
-    const response = await fetch(url, { method: 'GET' })
+    const TEMP_URL = getUrl(DEFAULT_SORT, USER_ADDRESS, 0, DEFAULT_PARTNER, DEFAULT_FILTER)
+    const response = await fetch(TEMP_URL, { method: 'GET' })
     const { counter } = await response.json()
 
-    for (var i = 0; i < counter; i++) {
-        console.log(`performing request ${i}`)
-        const response = await fetch(url, { method: 'GET' })
-        const json = await response.json()
-        robbies = [...robbies, ...json.data]
-    }
+    const start = new Date().getTime()
+    for (var page = 0; page < counter; page++) promises.push(request(page))
+    const result = await Promise.all(promises);
+    robbies = result.map(r => r.data)
+    robbies = [].concat.apply([], robbies);
+    const end = new Date().getTime();
 
+    // console.log(robbies)
     console.log(`Total robbies fetched: ${robbies.length}`)
+    console.log(`Call to [query] took ${end - start} milliseconds`)
 
 }
 
 query()
+// generateUrls(100)
 
 
 // robbie json example
